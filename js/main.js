718a104aa5fab13cb5b22b84538c438bccadd25f
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2021-10-22 14:21:15 -0400

    styled the webpage by sass modules

--- js/main.js
+++ js/main.js
@@ -6,21 +6,6 @@
 
   let desc = {};
 
-  // function getImage() {
-  //   fetch("./image-data.json")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-
-  //       buildImage(data);
-  //       // debugger;
-  //       let favImages = document.querySelectorAll(".fav-image");
-  //       imgClick(favImages);
-  //       // debugger;
-  //     })
-  //     .catch((error) => console.error(error));
-  // }
-
   function getDesc() {
     // debugger;
     fetch("./desc.json")
@@ -35,10 +20,8 @@
   }
 
   function buildDesc(event) {
-    debugger;
-    // const desc = Object.keys(description);
-    // const desc = "Valorant";
     let fId = event.target.parentElement.id;
+
     let desPanel = theFavDesTemplate.cloneNode(true);
 
     let desContainer = desPanel.firstElementChild.children;
@@ -50,64 +33,9 @@
     // desContainer[2].textContent = desc[2].fact;
     document.getElementById(fId).appendChild(desPanel);
     debugger;
+    // debugger;
   }
-  function addInfo() {
-    getDesc();
-    debugger;
-    buildDesc(desc);
-    console.log("addInfo fired");
-    // debugger;
 
-    // let key = this.dataset.key;
-    // debugger;
-    // let name = document.querySelector("h2");
-    // debugger;
-    // name.textContent =
-    // // name.textContent =
-  }
-  //   function builDesc(description) {
-  //     const desc = Object.keys(description);
-
-  //     desc.forEach((des) => {
-  //       let desPanel = theFavDesTemplate.cloneNode(true);
-
-  //       let desContainer = desPanel.firstElementChild.children;
-
-  //       desContainer[0].textContent = description[des].name;
-  //       desContainer[1].textContent = description[des].description;
-  //       desContainer[2].textContent = description[des].fact;
-
-  //       debugger;
-
-  //       theFav.appendChild(desPanel);
-  //     });
-  //   }
-
-  // function buildImage(info) {
-  //   const image = Object.keys(info);
-
-  //   image.forEach((source) => {
-  //     let imagePanel = theFavImgTemplate.cloneNode(true);
-
-  //     let imageContainer = imagePanel.children;
-
-  //     imageContainer[0].querySelector(
-  //       "img"
-  //     ).src = `images/${info[source].source}`;
-  //     debugger;
-  //     imageContainer[0].classList.add(`${source}`);
-  //     //   debugger;
-
-  //     theFav.appendChild(imagePanel);
-  //   });
-  // }
-
-  // function imgClick(favImages) {
-  //   favImages.forEach((img) => img.addEventListener("click", addInfo));
-  //   debugger;
-  // }
-
-  // getImage();
   getDesc();
   theFavThings.addEventListener("click", buildDesc);
 })();
